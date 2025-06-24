8e5a2b961372fe58871e582a8ae6edbdd0298cb9
Naveen8271 <dharavathanaveen@gmail.com>
2025-06-24 12:56:52 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,57 @@
+// let a =10;
+// let b =20;
+// console.log("Arithmetic opertors");
+// let sum=a+b;
+// let sub =a-b;
+// let mul =a*b;
+// let div=a/b;
+// let remainder=a%b;
+// let exp=a**b;
+// console.log("addtion:",sum);
+// console.log("sub:",sub);
+// console.log("multipaction:",mul);
+// console.log("division:",div);
+// console.log("remainder:",remainder);
+// console.log("exponent:",exp);
+
+// //2.assignment opertors
+// let x=10;
+// x +=6;
+// x -= 4;
+// x *=2;
+// x /=4;
+// x %=3;
+// console.log("assignment opertors");
+// console.log(x);
+// console.log("\n");
+
+// // 3.comparison operators
+// let isEqual =(a==b);
+// let isStricEqual =(a===b);
+// let isNotEqual =(a!=b);
+// let isStricNotEqual =(a!==b);
+// let isGreaterThan =(a>b);
+// let isLessThan=(a<b);
+// console.log("comparison operators");
+// console.log(isEqual);
+// console.log(isStricEqual);
+// console.log(isGreaterThan);
+// console.log(isNotEqual);
+// console.log(isStricNotEqual);
+
+// // 4.logical operators
+// let andOperator =(a >0&& b>0);
+// let orOpertor=(a>0|| b<0);
+// let notOpertor=!(a<b);
+// console.log("logical opertors");
+// console.log("AND",andOperator);
+// console.log("OR",orOpertor);
+// console.log("NOT",notOpertor);
+
+// //5.bitwise operators
+
+// 6.ternary operator
+let age=18;
+let canVote =(age>=18)?"yes you can vote":"no,cannot vote";
+console.log("teranary");
+console.log(canVote);
