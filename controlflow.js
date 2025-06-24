2b4c68948a4a06612b91a3dcf7e80bad6a350e44
Naveen8271 <dharavathanaveen@gmail.com>
2025-06-24 13:58:15 +0530

    Create controlflow.js

---
+++ controlflow.js
@@ -0,0 +1,40 @@
+
+// controll flow in Java
+let age=18;
+if(age <18){
+    console.log("You are a minor.");
+}
+else if(age >= 18 && age < 65){
+    console.log("You are an adult.");
+}
+else{
+    console.log("You are a senior citizen.");
+}
+
+// switch case example
+let day = 3; 
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    
+
+}
